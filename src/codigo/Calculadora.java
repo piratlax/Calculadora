a04b52448eeb60f1ecbb03f9197916ac3ab4ba75
Mao Rivera Sanchez Armas <piratlax@gmail.com>
2016-08-22 15:59:09 -0500

    Terminado lo grafico
    se ve raro, empezamos con la lógica de los botones

--- src/codigo/Calculadora.java
+++ src/codigo/Calculadora.java
@@ -18,14 +18,41 @@ class Marco extends JFrame{
 		setBounds (300,500,300,350);
 		Lamina lamina=new Lamina();
 		add(lamina);
+		
 	}
 }
 
 class Lamina extends JPanel{
+	JPanel milamina2=new JPanel();
+	
 	public Lamina(){
 		setLayout(new BorderLayout());
 		
 		JButton pantalla=new JButton("0");
 		add (pantalla,BorderLayout.NORTH);
+		pantalla.setEnabled(false);
+		milamina2.setLayout(new GridLayout(4,4));
+		ponerBoton("7");
+		ponerBoton("8");
+		ponerBoton("9");
+		ponerBoton("/");
+		ponerBoton("4");
+		ponerBoton("5");
+		ponerBoton("6");
+		ponerBoton("*");
+		ponerBoton("1");
+		ponerBoton("2");
+		ponerBoton("3");
+		ponerBoton("+");
+		ponerBoton("-");
+		ponerBoton("=");
+		
+		add(milamina2, BorderLayout.CENTER);
+		
+		
+	}
+	private void ponerBoton(String Rotulo){
+		JButton boton = new JButton(Rotulo);
+		milamina2.add(boton);
 	}
 }
